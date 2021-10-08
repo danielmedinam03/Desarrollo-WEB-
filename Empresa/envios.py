7dd0ebdc90cd3b4c521033f4abf97bf12bc0e99b
Daniel-Medina03 <daniel.felipe.medina.moreno@gmail.com>
2021-10-08 11:46:24 -0500

    Prueba del insert a la BD en Genero

--- Empresa/envios.py
+++ Empresa/envios.py
@@ -1,36 +1,62 @@
 from flask import Flask, render_template, request
+import os
+import pyodbc
 
 #instancia de flask
 app = Flask(__name__)
 
+# Ruta actual de la carpeta donde se esta trabajando
+# Conexion a la BD
+ruta = os.getcwd()
 
-@app.route('/')
+cadenaRutaBD = ruta + '\BD\EmpresaBD.accdb;'
+driver = "Microsoft Access Driver (*.mdb, *.accdb)"
+cadena = (r'DRIVER={};DBQ={}').format(driver, cadenaRutaBD)
+
+conexion = pyodbc.connect(cadena)
+
+@app.route('/', methods=["GET","POST"])
 def Index():
-   return render_template("index.html")
+    return render_template("index.html")
 
-@app.route('/tablas')
+@app.route('/tablas', methods=["GET","POST"])
 def Tablas():
-   return render_template("tablas.html")
+    return render_template("tablas.html")
 
-@app.route('/genero')
+@app.route('/genero', methods=["GET","POST"])
 def Genero():
-   return render_template("genero.html")
+    
+    codGenero = request.args.get("codGenero")
+    desGenero = request.args.get("desGenero")
 
-@app.route('/estadoCivil')
+    #codGenero=(request.form['codGenero'])
+    #desGenero=(request.form['desGenero'])
+    print(codGenero, desGenero)
+    
+    insert="INSERT INTO Genero(cCodigoGenero, cDescripcionGenero) VALUES('{}','{}')".format(codGenero, desGenero)
+    cursor = conexion.cursor()
+    cursor.execute(insert)
+    cursor.commit()
+    
+    if request.method == "POST":
+        return render_template("genero.html")
+    return render_template("genero.html")
+
+@app.route('/estadoCivil',methods=["GET","POST"])
 def EstadoCivil():
-   return render_template("estadoCivil.html")
+    return render_template("estadoCivil.html")
 
-@app.route('/estrato')
+@app.route('/estrato', methods=["GET","POST"])
 def Estrato():
-   return render_template("estrato.html")
+    return render_template("estrato.html")
 
-@app.route('/formaDePago')
+@app.route('/formaDePago', methods=["GET","POST"])
 def FormaDePago():
-   return render_template("formaDePago.html")
+    return render_template("formaDePago.html")
 
-@app.route('/tiposDeID')
+@app.route('/tiposDeID', methods=["GET","POST"])
 def TiposID():
-   return render_template("tiposDeID.html")
+    return render_template("tiposDeID.html")
 
     
 if __name__=='__main__':
