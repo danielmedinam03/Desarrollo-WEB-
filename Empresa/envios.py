f3ffe647311a56f86f90442f1b5015d1ad94ec02
Daniel-Medina03 <daniel.felipe.medina.moreno@gmail.com>
2021-10-08 10:55:06 -0500

    Rutas creadas en PY

--- Empresa/envios.py
+++ Empresa/envios.py
@@ -5,18 +5,33 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def genero():
-    
+def Index():
+   return render_template("index.html")
+
+@app.route('/tablas')
+def Tablas():
+   return render_template("tablas.html")
+
+@app.route('/genero')
+def Genero():
    return render_template("genero.html")
 
-@app.route('/genero', methods=['POST'])
-def Enviogenero():
-    if request.method == "POST":
-        
-        genero = request.form['codGenero'] 
-    
-        print(genero)
-        return "REcibido"
+@app.route('/estadoCivil')
+def EstadoCivil():
+   return render_template("estadoCivil.html")
+
+@app.route('/estrato')
+def Estrato():
+   return render_template("estrato.html")
+
+@app.route('/formaDePago')
+def FormaDePago():
+   return render_template("formaDePago.html")
+
+@app.route('/tiposDeID')
+def TiposID():
+   return render_template("tiposDeID.html")
+
     
 if __name__=='__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5500, debug=True)
